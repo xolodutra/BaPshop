baf51cd042f9410c0297d3e50cb7811f37e49e8d
xolodutra <pavelsahnov@yandex.ru>
2017-05-13 00:32:19 +0300

    Added a test product

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+''' Страница с товарами '''
+
+
+def 
